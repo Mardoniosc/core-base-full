15e010fb451693585f68fca9fb0aa18a34f89de5
programadorsemcortes <programadorsemcortes@gmail.com>
2020-11-04 12:41:39 -0300

    Criado os modulos de perfil e permissao

--- core-base-web/src/app/pages/permitions/permitions.module.ts
+++ core-base-web/src/app/pages/permitions/permitions.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PermitionsRoutingModule } from './permitions-routing.module';
+import { PermitionFormComponent } from './permition-form/permition-form.component';
+import { PermitionListComponent } from './permition-list/permition-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PermitionFormComponent, PermitionListComponent],
   imports: [
     CommonModule,
     PermitionsRoutingModule
--- core-base-web/src/app/pages/profiles/profiles.module.ts
+++ core-base-web/src/app/pages/profiles/profiles.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfilesRoutingModule } from './profiles-routing.module';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
+import { ProfileListComponent } from './profile-list/profile-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfileFormComponent, ProfileListComponent],
   imports: [
     CommonModule,
     ProfilesRoutingModule
