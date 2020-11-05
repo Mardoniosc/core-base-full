2188ad0bb28667abef8b805a7d6d23c161495bdd
Mardoniosc <mardonio@live.com>
2020-11-05 14:53:54 -0300

    Criando o componente formFieldError

--- core-base-web/src/app/shared/shared.module.ts
+++ core-base-web/src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BreadCrumbComponent } from './components';
-import { RouterModule } from '@angular/router'
+import { BreadCrumbComponent, FormFieldErrorComponent } from './components';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [BreadCrumbComponent],
+  declarations: [BreadCrumbComponent, FormFieldErrorComponent],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [
     // Módulos
@@ -14,6 +14,7 @@ import { RouterModule } from '@angular/router'
 
     // Components
     BreadCrumbComponent,
+    FormFieldErrorComponent,
   ],
 })
 export class SharedModule {}
