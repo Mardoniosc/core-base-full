bb1e5cee770f3a3cc217b698547eebdd673613b8
Mardoniosc <mardonio@live.com>
2020-11-05 13:12:08 -0300

    Utilizando o componente 'BreadCrumb'

--- core-base-web/src/app/pages/historico-log/historico-log.module.ts
+++ core-base-web/src/app/pages/historico-log/historico-log.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HistoricoLogRoutingModule } from './historico-log-routing.module';
 import { HistoryListComponent } from './history-list/history-list.component';
+import { SharedModule } from 'src/app/shared';
 
 
 @NgModule({
   declarations: [HistoryListComponent],
   imports: [
     CommonModule,
-    HistoricoLogRoutingModule
+    HistoricoLogRoutingModule,
+    SharedModule
   ]
 })
 export class HistoricoLogModule { }
